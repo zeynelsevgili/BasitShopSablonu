a8a27b4aaec1f49f67df7fd63dc113117505c277
smart <zeynel.sevgili@gmail.com>
2018-09-01 15:03:29 +0300

    Initial Commit

---
+++ BasitShopSablonu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BasitShopSablonu
+//
+//  Created by Demo on 1.09.2018.
+//  Copyright © 2018 Demo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
